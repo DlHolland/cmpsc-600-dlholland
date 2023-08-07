129a74b9d26f2b5bd0644424d35b18f9ac83f207
DlHolland <hettyist.dylan@gmail.com>
2023-08-07 12:39:14 -0400

    Add more to monofile

--- src/scripts/monofile.py
+++ src/scripts/monofile.py
@@ -1,11 +1,16 @@
 #! /home/dylan/.codon/bin/codon run
 
 from python import sys
+from python import subprocess
+from python import os
 from python import bandit.cli.main as bandit
 from python import flake8.main.cli as flake8
 from python import autoflake
 from python import eradicate
 from python import isort.main as isort
+from python import pydocstyle.__main__ as pydocstyle
+from python import tryceratops.__main__ as tryceratops
+from python import ruff.__main__ as ruff
 
 def reset_args():
     values = len(sys.argv) -1
@@ -60,6 +65,7 @@ def run():
         sys.argv.append("/home/dylan/codonDevs/monofile.py")
         eradicate.main()
     except PyError as e:
+        print(e)
         if str(e) == "0":
             pass
     reset_args()
@@ -71,6 +77,41 @@ def run():
         sys.argv.append("/home/dylan/codonDevs/monofile.py")
         isort.main()
     except PyError as e:
+        print(e)
+        if str(e) == "0":
+            pass
+    reset_args()
+
+    try:
+        # Pydocstyle
+        print("PYDOCSTYLE")
+        sys.argv.append("bandit-script.py")
+        pydocstyle.main()
+    except PyError as e:
+        print(e)
+        if str(e) == "0":
+            pass
+    reset_args()
+
+    try:
+        # Tryceratops
+        print("TRYCERATOPS")
+        sys.argv.append("bandit-script.py")
+        tryceratops.main()
+    except PyError as e:
+        print(e)
+        if str(e) == "0":
+            pass
+    reset_args()
+
+
+    try:
+        # Ruff
+        print("RUFF")
+        sys.argv.append("/home/dylan/codonDevs/bandit/bandit")
+        subprocess.run([os.fsdecode(ruff.find_ruff_bin()), *sys.argv[1:]])
+    except PyError as e:
+        print(e)
         if str(e) == "0":
             pass
     reset_args()
