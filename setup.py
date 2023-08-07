45e90a9d50c03b21420703848408f0a78d96ee1f
DlHolland <hettyist.dylan@gmail.com>
2023-08-07 11:55:31 -0400

    Add files and update monofile

---
+++ setup.py
@@ -0,0 +1,5 @@
+"""This is the setup file that will be used with my script."""
+
+
+dependency_name = "bandit"
+directory = "/test-base/bandit/"
--- src/scripts/monofile.py
+++ src/scripts/monofile.py
@@ -2,6 +2,7 @@
 
 from python import sys
 from python import bandit.cli.main as bandit
+from python import flake8.main.cli as flake8
 from python import autoflake
 from python import eradicate
 from python import isort.main as isort
@@ -11,6 +12,7 @@ def reset_args():
     while len(sys.argv) > 0:
         del sys.argv[values]
         values -= 1
+    sys.argv.append("")
     
 
 def run():
@@ -25,13 +27,26 @@ def run():
     except PyError as e:
         if str(e) == "0":
             pass
-    """
+    
     reset_args()
-    print(sys.argv)
+    """
+
+    try:
+        # flake8
+        print("FLAKEY")
+        sys.argv.append("/home/dylan/codonDevs")
+        flake8.main()
+    except PyError as e:
+        if str(e) == "0":
+            pass
+
+    reset_args()
+
     try:
         # Autoflake
-        sys.argv.append("")
-        sys.argv.append("../Documents/tests/autoflake-test.py")
+        print("AUTOFLAKE")
+        sys.argv.append("--in-place")
+        sys.argv.append("/home/dylan/codonDevs/monofile.py")
         autoflake.main()
     except PyError as e:
         if str(e) == "0":
@@ -41,8 +56,8 @@ def run():
 
     try:
         # Eradicate
-        sys.argv.append("")
-        sys.argv.append("../Documents/tests/eradicate.py")
+        print("ERADICATE")
+        sys.argv.append("/home/dylan/codonDevs/monofile.py")
         eradicate.main()
     except PyError as e:
         if str(e) == "0":
@@ -51,8 +66,9 @@ def run():
 
     try:
         # Isort
-        sys.argv.append("")
-        sys.argv.append("../Documents/bandit")
+        print("ISORT")
+        sys.argv.append("-v")
+        sys.argv.append("/home/dylan/codonDevs/monofile.py")
         isort.main()
     except PyError as e:
         if str(e) == "0":
