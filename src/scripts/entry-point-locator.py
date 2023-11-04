9b2d35476afa14e8bbfa30ae7dac302e2ecdf240
DlHolland <hettyist.dylan@gmail.com>
2023-11-03 23:23:11 -0400

    Add entry point locator

--- src/scripts/entry-point-locator.py
+++ src/scripts/entry-point-locator.py
@@ -1,14 +1,22 @@
+import importlib
+import pkg_resources
 
+# Create generator to store all CLI scripts
+pkgs = pkg_resources.iter_entry_points(group='console_scripts')
 
+# Store entry points; pkgs is a generator, so it exhausts itself
+entry_points = []
 
+# Iterate and store
+for pkg in pkgs:
+  entry_points.append(pkg)
 
-def main():
+bandit_entry = None
+for entry in entry_points:
+  if entry.name == "bandit":
+    bandit_entry = entry.module_name
 
+print(bandit_entry)
 
-    
-
-    return
-
-
-if __name__ == "__main__":
-    main()
+mod = importlib.import_module(bandit_entry)
+mod.main()
